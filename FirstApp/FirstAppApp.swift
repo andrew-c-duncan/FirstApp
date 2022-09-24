ccf848ba461a45060aff421c11563a09f78bac1e
andrew-c-duncan <adaw@bigpond.com>
2022-09-24 10:46:01 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Andrew Duncan on 24/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
